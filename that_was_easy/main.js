387ecf5dc707ae9d341653f2137e4312cdcdcd8e
Xiomara Cruz <cruzxiomara91@gmail.com>
2017-01-23 20:08:45 +0000

    Add geometric_patterns project

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -4,3 +4,11 @@ thatWasEasy.play();
 }
 
 $("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+        $("#easy").trigger("click")
+    }
+}
