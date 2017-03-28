347081baac68b74e84b7a417d9d662f7c1052099
Xiomara Cruz <cruzxiomara91@gmail.com>
2017-03-28 18:47:50 +0000

    fixed dashboard

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -1,50 +1,50 @@
 function loadDate() {
-    var currentDate = new Date();
-    var dateString = currentDate.toString()
-    .split(" ")
-    .splice(0, 4)
-    .join(" ");
-    
-    $("#date").text(dateString);
+  var currentDate = new Date();
+  var dateString = currentDate.toString()
+                     .split(" ")
+                     .splice(0, 4)
+                     .join(" ");
+
+  $("#date").text(dateString);
 }
 
 function loadWeather() {
-    var weather = $("#weather");
-    var url = "https://api.forecast.io/forecast/";
-    var apiKey = "7f27815af57ebfbc50cb923890bf6d48"; 
+  var weather = $("#weather");
+  var url = "https://api.forecast.io/forecast/";
+  var apiKey = "2b03a2892a6d3a139b601dc247b53ede";
 
-function success(position) {
-    var latitude = position.coords.latitde;
+  function success(position) {
+    var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    
+
     $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-        weather.text("Based on your current location, it is " + data.currently.temperature + "° F right now");
+      weather.text("Based on your current location, it is " + data.currently.temperature + "° F right now");
     });
-}
+  }
 
-function error() {
+  function error() {
     alert("Unable to retrieve your location for weather");
-}
+  }
 
-navigator.geolocation.getCurrentPosition(success, error);
+  navigator.geolocation.getCurrentPosition(success, error);
 
-weather.text("fetching weather...");
+  weather.text("fetching weather...");
 }
 
 function loadNews() {
-    var news = $("#news");
-    var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
-    var apiKey = "03bc5e81feab4f688d8d4bea8e5a0a1d";
-    
-    $.getJSON(url + apiKey, function(data) {
+  var news = $("#news");
+  var url = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=";
+  var apiKey = "6628f0304dd549b69436dc1c2880054e";
+
+  $.getJSON(url + apiKey, function(data) {
     var titles = data.articles.map(function(articles) {
-            return "<a href='" + articles.url + "'>" + articles.title + "</a>";
-        });
-        
-        news.html(titles.join("<br><br>"));
+      return "<a href='" + articles.url + "'>" + articles.title + "</a>";
     });
-    
-    news.text("fetching news...");
+
+    news.html(titles.join("<br><br>"));
+  });
+
+  news.text("fetching news...");
 }
 
 loadDate();
